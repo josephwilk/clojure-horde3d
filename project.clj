b6a91c975bbb7436ea2d6a7d5185f261aab15bb5
Joseph Wilk <joe@josephwilk.net>
2014-09-19 12:24:31 -0500

    skele

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject horde3d "0.1.0-SNAPSHOT"
+  :description "Can Clojure and Horde3d play?"
+  :url "https://github.com/josephwilk/clojure-horde3d"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ src/horde3d/core.clj
@@ -0,0 +1,6 @@
+(ns horde3d.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
